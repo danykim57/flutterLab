98d493184740a9782a2a19d1dde643aeb79ce1bc
unknown <fhtmah12@gmail.com>
2022-08-09 22:05:04 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Aimport 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
