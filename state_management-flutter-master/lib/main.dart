4afc44c734fab4962913d43396690a4edb2a385b
Yeongin Kim <fhtmah12@gmail.com>
2022-08-31 21:07:39 +0900

    state management edited

--- state_management-flutter-master/lib/main.dart
+++ state_management-flutter-master/lib/main.dart
@@ -1,76 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Data>(
-      builder: (context) => Data(),
+    return ChangeNotifierProvider(
+      builder: (context) => TaskData(),
       child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: MyText(),
-          ),
-          body: Level1(),
-        ),
+        home: TasksScreen(),
       ),
     );
-  }
-}
-
-class Level1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Level2(),
-    );
-  }
-}
-
-class Level2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MyTextField(),
-        Level3(),
-      ],
-    );
-  }
-}
-
-class Level3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context).data);
-  }
-}
-
-class MyText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context, listen: false).data);
-  }
-}
-
-class MyTextField extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      onChanged: (newText) {
-        Provider.of<Data>(context).changeString(newText);
-      },
-    );
-  }
-}
-
-class Data extends ChangeNotifier {
-  String data = 'Some data';
-
-  void changeString(String newString) {
-    data = newString;
-    notifyListeners();
   }
 }
